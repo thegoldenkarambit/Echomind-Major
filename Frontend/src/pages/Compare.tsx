db1515f7a2075e4744be15c0f8cfd0a2fa8eeecf
thegoldenkarambit <anujsingh.9feb@gmail.com>
2026-03-06 21:19:56 +0530

    Connected frontend to Render backend

--- Frontend/src/pages/Compare.tsx
+++ Frontend/src/pages/Compare.tsx
@@ -95,7 +95,7 @@ const Compare = () => {
 
     setIsLoading(true);
 
-    const res = await fetch("http://127.0.0.1:8000/compare", {
+    const response = await fetch("https://echomind-backend.onrender.com/compare", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt })
